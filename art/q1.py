d4962d5d0c33e7d496665d8c939530a477605d29
quadrohedron <quadrohedron@gmail.com>
2014-07-03 12:25:17 +0100

    Running art/q1.py

--- art/q1.py
+++ art/q1.py
@@ -1,1 +1,21 @@
-print "A new file!"
+from tealight.art import (color, line, spot, circle, box, image, text, background)
+
+from tealight.art import (screenWidth, screenHeight)
+
+from math import *
+
+print "This is art mode!"
+
+
+print screenWidth
+print screenHeight
+
+spot(0,10,20)
+
+for i in range(10,screenWidth,20):
+  for j in range(10,screenHeight,20):
+    circle(i,j,10)
+
+def handle_mousemove(x,y):
+  if hypot((x+10)%20,(y+10)%20)<=20:
+    spot((x//20)*20+10,(y//20)*20+10,10)
